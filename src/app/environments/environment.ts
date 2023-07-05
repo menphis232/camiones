f7ea529260cc63bee350db856ec6ecfd435eb730
DouglasCollura <douglascollura@gmail.com>
2023-07-05 08:12:01 -0400

    change env

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://24.199.110.103:5000/api',
+    // apiUrl: 'http://24.199.110.103:5000/api',
+    apiUrl:'https://camiones.pruebasbackenddattatech.online/api'
     // otras variables de entorno específicas para desarrollo
   };
